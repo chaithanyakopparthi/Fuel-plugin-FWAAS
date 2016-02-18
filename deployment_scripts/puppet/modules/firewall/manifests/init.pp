9f1de416a03cb68d26d283b61c6fc0550696efc8
chaithanyakopparthi <chaithanyak@vedams.com>
2016-02-18 15:46:53 +0530

    Update init.pp

--- deployment_scripts/puppet/modules/firewall/manifests/init.pp
+++ deployment_scripts/puppet/modules/firewall/manifests/init.pp
@@ -10,33 +10,33 @@ class firewall{
     ensure => present,
     line   => 'service_provider = FIREWALL:Iptables:neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver:default',
     path   => '/etc/neutron/neutron.conf',
-  require => Ini_setting["add_fwaas_service_plugin"],}
+  require => Ini_setting['add_fwaas_service_plugin'],}
   ini_setting {'enable_neutron_driver':
     ensure => present,
     section => 'fwaas',
     path => '/etc/neutron/fwaas_driver.ini',
     setting => 'driver',
     value => 'neutron.services.firewall.drivers.linux.iptables_fwaas.IptablesFwaasDriver',
-  require => File_line["service_provider"],}
+  require => File_line['service_provider'],}
   ini_setting {'enable_neutron':
     ensure => present,
     section => 'fwaas',
     path => '/etc/neutron/fwaas_driver.ini',
     setting => 'endbled',
     value => 'True',
-  require => Ini_setting["enable_neutron_driver"],}
+  require => Ini_setting['enable_neutron_driver'],}
   exec { 'enable_fwaas_dashboard':
     command => "/bin/sed -i \"s/'enable_firewall': False/'enable_firewall': True/\" /usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.py",
     unless => "/bin/egrep \"'enable_firewall': True\" /usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.py",
-  require => Ini_setting["enable_neutron"],}
-  service { "neutron-server":
-    ensure => "running",
-    enable => "true",
-  require => Exec["enable_fwaas_dashboard"],}
-  service { "apache2":
-    ensure => "running",
-    enable => "true",
-  require => Service["neutron-server"],}
+  require => Ini_setting['enable_neutron'],}
+  service { 'neutron-server':
+    ensure => 'running',
+    enable => true,
+  require => Exec['enable_fwaas_dashboard'],}
+  service { 'apache2':
+    ensure => 'running',
+    enable => true,
+  require => Service['neutron-server'],}
 }
 
 
